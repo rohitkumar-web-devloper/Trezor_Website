787c5089d74e2ccf243e204a543bc500b4dd5733
rohitkumar-web-devloper <113985385+rohitkumar-web-devloper@users.noreply.github.com>
2025-10-11 14:58:18 +0530

    some changes

--- components/home/ConnectCard.tsx
+++ components/home/ConnectCard.tsx
@@ -2,7 +2,8 @@
 import { useEffect, useState } from "react";
 import ErrorIcon from "../ErrorIcon";
 import LoadingSpinner from "../LoadingSpinner";
-
+import Image from "next/image";
+import trezor from '../../assets/trezor.png'
 const ConnectCard = ({ onConnectionError }: { onConnectionError: () => void }) => {
     const [status, setStatus] = useState('waiting');
     const [buttonText, setButtonText] = useState('Waiting for Trezor...');
@@ -30,14 +31,16 @@ const ConnectCard = ({ onConnectionError }: { onConnectionError: () => void }) =
     const handleRetry = () => {
         runConnectionSimulation();
     };
-
+ 
     return (
         <div className="flex flex-col items-center">
             <div className="flex h-[122px] w-[360px] max-w-full rounded-full p-2.5 bg-white items-center shadow-lg mb-16">
                 <div className="flex-shrink-0 h-[100px] w-[100px] rounded-full bg-gray-100 flex items-center justify-center overflow-hidden">
                     {/* The animated SVG from the original file can be complex to replicate. 
                     A simple loading indicator is used here. */}
-                    <div className="w-full h-full bg-gray-100" />
+                    <div className="w-full h-full bg-gray-100" >
+                        <Image src={trezor} alt="Logo"/>
+                    </div>
                 </div>
                 <div className="flex flex-col mx-8 text-center text-gray-800 text-xl font-medium">
                     <span>Connect your Trezor</span>
